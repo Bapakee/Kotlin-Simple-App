e9b2172d5129ce2b65bfffa05165966349df7d66
Reynald David Oenata <renaldavidoenata12@gmail.com>
2025-06-10 07:33:27 +0700

    Plugin update (app)

--- app/src/main/java/com/example/anmproject/MainActivity.kt
+++ app/src/main/java/com/example/anmproject/MainActivity.kt
@@ -5,11 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.anmproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
+
 }
